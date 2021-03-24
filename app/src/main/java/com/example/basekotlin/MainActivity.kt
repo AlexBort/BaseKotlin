1958efa9045fb2d01f9fa6d7f52f70b924376a05
oleksandr.bortnyk <oleksandr.bortnyk@vimmi.net>
2021-03-24 18:08:04 +0200

    DevColibry: Lesson3 - types of vars

--- app/src/main/java/com/example/basekotlin/MainActivity.kt
+++ app/src/main/java/com/example/basekotlin/MainActivity.kt
@@ -7,5 +7,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // var - ячейка памяти, в которую можно складывать какие-то значения
+        //
+        var a1: String = "123" // динамический тип данных, поэтому разные строки будут занимать разное количество памяти
+
+        var a0: Byte = 12 // 8
+        var a2: Int = 123 // статический тип- 32 бита
+        var a3: Long = 123 // также статический тип - 64
+
+        var a4: Double = .1 // 64
+        var a5: Float = .1F // 32
+//        Эти типы не есть примитивами, а являются классами
+
+        var convertVar: Long = a2.toLong()
     }
 }
