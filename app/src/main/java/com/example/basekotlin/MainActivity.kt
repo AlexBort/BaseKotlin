0291fd512dff398bc7b967443d91557f1027d3e7
oleksandr.bortnyk <oleksandr.bortnyk@vimmi.net>
2021-03-24 18:51:19 +0200

    DevColibry: Lesson5 - Null pointer safety; null pointer exception

--- app/src/main/java/com/example/basekotlin/MainActivity.kt
+++ app/src/main/java/com/example/basekotlin/MainActivity.kt
@@ -7,5 +7,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val str: String = ""
+
+        val strNull: String?
+        strNull = getStrNull()
+
+        val size = strNull!!.length // should write "!!" because it waits that we can get null, therefore we should say that we are confident that strNull not be null
+        println(size) // it will be crash- KotlinNullPointerException
+        // это все помогает нам обезопаситься с тем, чтобы не создать nullPointerException
+
+        // Но мы можем не понять или null, или нет, если мы будем вызывать джавовский класс
+    }
+
+    fun getStrNull(): String? {
+        return null
     }
 }
