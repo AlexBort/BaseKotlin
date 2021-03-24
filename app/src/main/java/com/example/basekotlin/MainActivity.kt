db9d4c620eb08128446670ceab4d77e3b244bdf2
oleksandr.bortnyk <oleksandr.bortnyk@vimmi.net>
2021-03-24 17:17:40 +0200

    DevColibry: Lesson1 - String templates

--- app/src/main/java/com/example/basekotlin/MainActivity.kt
+++ app/src/main/java/com/example/basekotlin/MainActivity.kt
@@ -7,5 +7,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var name: String = "Alex"
+        println("Hello $name")// it will understand it as var
+        println("Hello \$name") // it will understand it as word "name" not var
+
+        var var1: Int = 1
+        var var2: Int = 2
+        println("var1 + var2= ${var1 + var2} ") // it will understand as sum of this Int vars
+
     }
 }
