33541619f9658835cf7c30481fea233c2069542d
oleksandr.bortnyk <oleksandr.bortnyk@vimmi.net>
2021-03-28 23:48:18 +0300

    DevColibry: Lesson8 - Enum classes

---
+++ app/src/main/java/com/example/basekotlin/Colors.kt
@@ -0,0 +1,10 @@
+package com.example.basekotlin
+
+enum class Colors(val colorCode: String, val opacity: Double? = 1.0) {
+    // enum - это перечисление констатных значений, которые мы знаем зраннее чему они равны
+
+    WHITE(colorCode = "",2.0),
+    BLACK(colorCode = ""),
+    RED(colorCode = ""),
+    GREEN(colorCode = "")
+}
--- app/src/main/java/com/example/basekotlin/MainActivity.kt
+++ app/src/main/java/com/example/basekotlin/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.basekotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.v("","RED")
+        Log.v("","BLACK")
+        Log.v("","GREEN")
+
+        println(Colors.BLACK)
+        println(Colors.GREEN)
+        println(Colors.RED)
+        println(Colors.WHITE)
+
     }
 }
