52d43a76dec0a6ce0c20bef7a22ba558afe3c381
Oleksandr Bortnyk <oleksandr_bortnyk@epam.com>
2022-02-20 16:10:55 +0200

    variant with simple approach of MVI: added binding for fragments

--- app/src/main/java/com/example/basekotlin/mvi_simple_approach/screens/ListFragment.kt
+++ app/src/main/java/com/example/basekotlin/mvi_simple_approach/screens/ListFragment.kt
@@ -5,15 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.basekotlin.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+
+    private var _binding: FragmentListBinding? = null
+
+    // This property is only valid between onCreateView and
+// onDestroyView.
+    private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
 
