9f78e57342aa9888cfcbc71bbd790368555f3f4c
Oleksandr Bortnyk <oleksandr_bortnyk@epam.com>
2022-05-22 20:59:34 +0300

    efforts

--- app/src/main/java/com/example/basekotlin/MainActivity.kt
+++ app/src/main/java/com/example/basekotlin/MainActivity.kt
@@ -4,21 +4,39 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import com.example.basekotlin.databinding.ActivityMainBinding
+//import com.example.basekotlin.databinding.ActivityMainBinding
+import com.example.basekotlin.databinding.TestMainActivityBinding
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+//    private lateinit var binding: TestMainActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+//        binding = TestMainActivityBinding.inflate(layoutInflater)
         val rootView = binding.root
         setContentView(rootView)
+
+//        supportActionBar?.apply {
+//            setDisplayShowHomeEnabled(true)
+//            setDisplayHomeAsUpEnabled(true)
+//            setHomeButtonEnabled(true)
+//        }
+        binding.toolbar.setTitle(R.string.test_name_toolbar)
+        binding.toolbar.apply {
+            inflateMenu(R.menu.test_menu)
+        }
+        binding.toolbar.setNavigationIcon(R.drawable.ic_baseline_clear_24)
+        // todo: to add close button
+        setSupportActionBar(binding.toolbar)
+
+// to add list with items
+
 //        setSupportActionBar(binding.customToolbar.supportToolbar)
 //        setContentView(R.layout.activity_main)
-
-
 
 
     }
